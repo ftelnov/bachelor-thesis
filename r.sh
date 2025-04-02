4cff0ad7b2bfdca7183a8fe72c27a603ccfcadde
Fedor Telnov <ftelnov@protonmail.com>
2025-04-02 12:27:07 +0300

    fix build script, add manual bibliography

--- r.sh
+++ r.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+set -e
 
 BUILD_DIR="$PWD/build"
-if [[ "$1" == "clean" ]]; then
-    rm -f $BUILD_DIR/{bachelor-thesis,master-thesis,master-thesis-en}.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
-else
-    cd src
-    for i in bachelor-thesis ; do
-        xelatex --halt-on-error -output-directory $BUILD_DIR $i
-        biber --halt-on-error -output-directory $BUILD_DIR $i
-        xelatex --halt-on-error -output-directory $BUILD_DIR $i
-        xelatex --halt-on-error -output-directory $BUILD_DIR $i
-    done
+ITEM="bachelor-thesis"
+cd build
 
-    rm -f $BUILD_DIR/{bachelor-thesis,master-thesis,master-thesis-en}.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
-fi
+rm -rf ./$ITEM
+cp -r ../src ./$ITEM
+cd $ITEM
+
+xelatex --halt-on-error $ITEM
+biber $ITEM
+xelatex --halt-on-error $ITEM
+xelatex --halt-on-error $ITEM
